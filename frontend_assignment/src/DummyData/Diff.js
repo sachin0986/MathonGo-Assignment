f4974f0e58774238fd36d95c84a5bdc138dceb92
Sachin Arora <sachinarora0986@gmail.com>
2025-06-05 18:57:16 +0530

    base created

---
+++ frontend_assignment/src/DummyData/Diff.js
@@ -0,0 +1,112 @@
+[
+    {
+    "subject": "Physics",
+    "chapter": "Mathematics in Physics",
+    "class": "Class 11",
+    "unit": "Mechanics 1",
+    "yearWiseQuestionCount": {
+      "2019": 0,
+      "2020": 2,
+      "2021": 5,
+      "2022": 5,
+      "2023": 3,
+      "2024": 7,
+      "2025": 6
+    },
+    "questionSolved": 0,
+    "status": "Not Started",
+    "isWeakChapter": false
+  },
+  {
+    "subject": "Physics",
+    "chapter": "Units and Dimensions",
+    "class": "Class 11",
+    "unit": "Mechanics 1",
+    "yearWiseQuestionCount": {
+      "2019": 2,
+      "2020": 6,
+      "2021": 8,
+      "2022": 4,
+      "2023": 6,
+      "2024": 3,
+      "2025": 10
+    },
+    "questionSolved": 39,
+    "status": "Completed",
+    "isWeakChapter": true
+  },
+
+    {
+    "subject": "Chemistry",
+    "chapter": "Classification of Elements and Periodicity in Properties",
+    "class": "Class 11",
+    "unit": "Inorganic Chemistry",
+    "yearWiseQuestionCount": {
+      "2019": 4,
+      "2020": 8,
+      "2021": 1,
+      "2022": 2,
+      "2023": 3,
+      "2024": 9,
+      "2025": 5
+    },
+    "questionSolved": 0,
+    "status": "Not Started",
+    "isWeakChapter": true
+  },
+  {
+    "subject": "Chemistry",
+    "chapter": "Chemical Bonding and Molecular Structure",
+    "class": "Class 11",
+    "unit": "Inorganic Chemistry",
+    "yearWiseQuestionCount": {
+      "2019": 7,
+      "2020": 5,
+      "2021": 2,
+      "2022": 0,
+      "2023": 5,
+      "2024": 5,
+      "2025": 3
+    },
+    "questionSolved": 22,
+    "status": "In Progress",
+    "isWeakChapter": false
+  },
+
+    {
+    "subject": "Mathematics",
+    "chapter": "Basic of Mathematics",
+    "class": "Class 11",
+    "unit": "Miscellaneous",
+    "yearWiseQuestionCount": {
+      "2019": 9,
+      "2020": 9,
+      "2021": 6,
+      "2022": 5,
+      "2023": 1,
+      "2024": 1,
+      "2025": 8
+    },
+    "questionSolved": 21,
+    "status": "In Progress",
+    "isWeakChapter": true
+  },
+  {
+    "subject": "Mathematics",
+    "chapter": "Quadratic Equation",
+    "class": "Class 11",
+    "unit": "Algebra",
+    "yearWiseQuestionCount": {
+      "2019": 1,
+      "2020": 6,
+      "2021": 1,
+      "2022": 1,
+      "2023": 9,
+      "2024": 1,
+      "2025": 1
+    },
+    "questionSolved": 0,
+    "status": "Not Started",
+    "isWeakChapter": false
+  },
+]
